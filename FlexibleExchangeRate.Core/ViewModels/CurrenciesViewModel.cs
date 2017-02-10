c7639559273bcbf82783ab4c9ac6b2932b1470ac
Michal Orzechowski <michal@orzechowski.info.pl>
2017-02-10 22:15:39 +0100

    Core and Droid projects

---
+++ FlexibleExchangeRate.Core/ViewModels/CurrenciesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexibleExchangeRate.Core.ViewModels
+{
+    public class CurrenciesViewModel
+        : MvxViewModel
+    {
+
+    }
+}
